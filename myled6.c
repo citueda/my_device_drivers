a28b75d5d22f1b05d971b5a0ff630c408043eff6
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-09-15 20:42:23 +0000

    Add the device file part

--- myled6.c
+++ myled6.c
@@ -11,8 +11,6 @@ MODULE_VERSION("0.1");
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
-static spinlock_t spn_lock;
-static int access_num = 0;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
@@ -20,36 +18,10 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
         return 1;
 }
 
-static int led_open(struct inode* inode, struct file* filp)
-{
-	spin_lock(&spn_lock);
-
-	if(access_num){
-		spin_unlock(&spn_lock);
-		return -EBUSY;
-	}
-
-	access_num++;
-	spin_unlock(&spn_lock);
-
-	return 0;
-}
-
-static int led_release(struct inode* inode, struct file* filp)
-{
-	spin_lock(&spn_lock);
-	access_num--;
-	spin_unlock(&spn_lock);
-
-	return 0;
-}
-
 static struct file_operations led_fops =
 {
 	owner   : THIS_MODULE,
 	write   : led_write,
-	open    : led_open,
-	release : led_release,
 };
 
 static int __init init_mod(void)
@@ -74,7 +46,6 @@ static int __init init_mod(void)
 		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
 	}
-	device_create(cls, NULL, MKDEV(MAJOR(dev),0), NULL, "myled0");
 
 	return 0;
 }
@@ -82,7 +53,6 @@ static int __init init_mod(void)
 static void __exit cleanup_mod(void)
 {
 	cdev_del(&cdv);
-	device_destroy(cls, MKDEV(MAJOR(dev),0));
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
