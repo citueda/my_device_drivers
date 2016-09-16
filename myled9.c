e10c6b8f4a62314a33aef05df79a4faafe80ddbd
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-09-16 18:26:15 +0000

    Add LED procedures

--- myled9.c
+++ myled9.c
@@ -3,7 +3,6 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <asm/uaccess.h>
-#include <linux/io.h>
 
 MODULE_AUTHOR("Ryuichi Ueda");
 MODULE_DESCRIPTION("driver for LED control");
@@ -13,8 +12,8 @@ MODULE_VERSION("0.1");
 static dev_t dev;
 static struct cdev cdv;
 static struct class *cls = NULL;
-
-static volatile uint32_t *gpio_base = NULL;
+static spinlock_t spn_lock;
+static int access_num = 0;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
@@ -22,35 +21,45 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
 
-	if(c == '0')
-		gpio_base[10] = (1 << 25) & 0xffffffff; //10: GPCLR0
-	else if(c == '1')
-		gpio_base[7] = (1 << 25) & 0xffffffff; //7: GPSET0
-
+	printk(KERN_INFO "receive %c\n",c);
         return 1;
+}
+
+static int led_open(struct inode* inode, struct file* filp)
+{
+	spin_lock(&spn_lock);
+
+	if(access_num){
+		spin_unlock(&spn_lock);
+		return -EBUSY;
+	}
+
+	access_num++;
+	spin_unlock(&spn_lock);
+
+	return 0;
+}
+
+static int led_release(struct inode* inode, struct file* filp)
+{
+	spin_lock(&spn_lock);
+	access_num--;
+	spin_unlock(&spn_lock);
+
+	return 0;
 }
 
 static struct file_operations led_fops =
 {
 	owner   : THIS_MODULE,
 	write   : led_write,
+	open    : led_open,
+	release : led_release,
 };
 
 static int __init init_mod(void)
 {
 	int retval;
-
-	const uint32_t rpi_gpio_base = 0x3f000000 + 0x200000;//レジスタのベース+ GPIOのオフセット
-	const uint8_t gpio_size = 0xC0;
-
-	const int led = 25;
-	const int index = 0 + led/10;//0: GPFSEL0, 25: GPIO NO
-	const int shift = (led%10)*3;
-	const uint32_t mask = ~(0x7 << shift);
-
-	gpio_base = ioremap_nocache(rpi_gpio_base, gpio_size); //0xC0: gpio size
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift); //0x1: GPF_OUTPUT
-
 	retval =  alloc_chrdev_region(&dev, 0, 1, "led");
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
@@ -82,7 +91,6 @@ static void __exit cleanup_mod(void)
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
-	iounmap(gpio_base);
 }
 
 module_init(init_mod);
